0b0707dbb078929ebe114a9aa63651e1a8608d9f
Dan Ventura <danwventura@icloud.com>
2016-02-27 09:49:26 -0600

    added event listener in main.js and lexicon in italian.js

--- italian.js
+++ italian.js
@@ -1,9 +1,41 @@
-var translator = (function () {
-var lexicon = ["happy": "felice", "fourth" : "quarto", "of" : "di", "july" : "luglio", "go" : "vai", "out" : "fuori", "and" : "e", "blow" : "colpo", "shit" : "merda", "up" : "fino", "birthday" : "compleanno", "greetings" : "saluti", "seasons" : "stagioni", "mother's" : "di madre", 
-"day" : "giorno", "father's" : "di father", "sit" : "sedersi", "your" : "il tuo", "ass" : "culo", "down" : "verso il basso", "where" : "dove", "is" : "è", "the" : "il", "rum" : "Rum", "Trump's" : "di Trump", "hair" : "capelli", "looks" : "sguardi", "like" : "come", "a" : "un", "dead" : "morti", "ferret" : "furetto", ]
+var Translator = (function () {
+var lexicon = {
+"happy": "felice", 
+"fourth" : "quarto", 
+"of" : "di",
+"july" : "luglio", 
+"go" : "vai", 
+"out" : "fuori", 
+"and" : "e", 
+"blow" : "colpo", 
+"shit" : "merda", 
+"up" : "fino", 
+"birthday" : "compleanno", 
+"greetings" : "saluti", 
+"seasons" : "stagioni", 
+"mother's" : "di madre", 
+"day" : "giorno", 
+"father's" : "di father", 
+"sit" : "sedersi", 
+"your" : "il tuo", 
+"ass" : "culo", 
+"down" : "verso il basso", 
+"where" : "dove", 
+"is" : "è", 
+"the" : "il", 
+"rum" : "Rum", 
+"Trump's" : "di Trump", 
+"hair" : "capelli", 
+"looks" : "sguardi", 
+"like" : "come", 
+"a" : "un", 
+"dead" : "morti", 
+"ferret" : "furetto" 
+}
 
 
-})();
+
+}();)
 
 
 
--- main.js
+++ main.js
@@ -2,5 +2,9 @@ var Translator = (function() {
   return {};
 }();)
 
-// This is where the event listeners will go
-
+var textInput = document.getElementById("Words");
+textInput.addEventListener("click", function(){
+  var inputString = textInput.value
+  // return inputString
+console.log("input",inputString );
+})
