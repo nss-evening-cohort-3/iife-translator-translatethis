0d2070f74bda6ae4bb8fa74b27c4a62ff92778fd
Dan Ventura <danwventura@icloud.com>
2016-02-26 20:31:56 -0600

    added lexicon variable

---
+++ italian.js
@@ -0,0 +1,9 @@
+var translator = (function () {
+var lexicon = ["happy": "felice", "fourth" : "quarto", "of" : "di", "july" : "luglio", "go" : "vai", "out" : "fuori", "and" : "e", "blow" : "colpo", "shit" : "merda", "up" : "fino", "birthday" : "compleanno", "greetings" : "saluti", "seasons" : "stagioni", "mother's" : "di madre", 
+"day" : "giorno", "father's" : "di father", "sit" : "sedersi", "your" : "il tuo", "ass" : "culo", "down" : "verso il basso", "where" : "dove", "is" : "è", "the" : "il", "rum" : "Rum", "Trump's" : "di Trump", "hair" : "capelli", "looks" : "sguardi", "like" : "come", "a" : "un", "dead" : "morti", "ferret" : "furetto", ]
+
+
+})();
+
+
+
