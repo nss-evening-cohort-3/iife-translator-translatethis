8b050e1f858630c5c175e155602c5c8f767f909b
Odie Joseph <Odie Joseph>
2016-02-27 09:49:34 -0600

    Haitian Creole Augmentor

---
+++ HaitianCreole.js
@@ -0,0 +1,24 @@
+// Create function to augment original translater
+// create object whose properties are the words
+var Translator = (function(augmentTranslator) {
+
+	var lexicon = {
+	"bon Katriyèm Jiye":"happy fourth of july"
+	"Ale":"go out", 
+	"ak":"and", 
+	"kònen kaka moute":"blow shit up", 
+	"fet":"birthday", 
+	"bonjour":"greetings", 
+	"sezon":"seasons", 
+	"jou manman an":"mother's day", 
+	"papa an":"father's", 
+	"poze boudou la":"sit your ass down", 
+	"kote wonm nan":"where is the rum", 
+	"cheve Trump an sanble yon découvrir mouri":"Trump's hair looks like a dead ferret"
+
+}
+	augmentTranslator.getHaitianCreole = function() {
+		return haitianCreole
+	};
+	)
+}(Translator || {} ));
