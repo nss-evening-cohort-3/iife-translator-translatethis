29a2b5934d72037d82520ed8addd3d680ec06a93
complikatyed <Katye.Russell@gmail.com>
2016-02-27 15:59:26 -0600

    Refactoring to make event listeners cooperate with each other

--- main.js
+++ main.js
@@ -1,32 +1,51 @@
+// This is the main iife that gets augmented by our individual js files
+
 var Translator = (function() {
   return {};
 }());
 
-var textInput = document.getElementById("Words");
+
+// All the things that we need to grab from the DOM in index.html
+
 var button = document.getElementById("btn");
-button.addEventListener("click", function pullValue(){
-  var inputString = textInput.value;
-
-  makeArray(inputString);
-});
-
-var makeArray = function (inputString) {
-  var inputArray = inputString.split(" ");
-
-  return inputArray;
-};
-
-
 var select = document.getElementById("languages");
+var textInput = document.getElementById("words");
+var languageChoice;
+
 
 select.addEventListener('change', function(){
   var languageChoice = select.selectedIndex;
-      chooseLanguage(languageChoice);
+  return languageChoice;
   });
 
-var chooseLanguage = function(languageSelection) {
+
+// This function takes the user's input and splits it into an array
+
+var makeArray = function(inputString) {
+  var inputArray = inputString.split(" ");
+};
+
+button.addEventListener("click", function(){
+  getInput();
+  console.log(languageChoice);
+});
+
+var getInput = function() {
+  var inputString = textInput.value;
+  makeArray(inputString);
+};
+
+// This function is called by the language selector event listener.
+// It calls a different function depending on what language is chosen.
+var select = document.getElementById("languages");
+
+
+
+var translateByLanguage = function(languageSelection, inputArray) {
+
   if (languageSelection === 1) {
     // This will be a call to the Haitian-Creole iife
+    console.log("Haitian", inputArray);
   }
   else if (languageSelection === 2) {
     // This will be a call to the Russian iife
@@ -36,8 +55,18 @@ var chooseLanguage = function(languageSelection) {
   }
   else if (languageSelection === 4) {
     // This will be a call to the Dutch iife
+
   }
   else {
     alert("I'm sorry.  That's not a valid choice.");
   }
 };
+
+
+// Grabbing the id where the text has been input, to get their value
+
+
+
+
+
+
